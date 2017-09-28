b2c95607bf791008709496e595dfae20b8e0d277
Tim Lund <lund.tim@gmail.com>
2017-09-28 09:20:54 -0600

    Chapter 9 End

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,70 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask_login import LoginManager
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
+from mockdbhelper import MockDBHelper as DBHelper
+from passwordhelper import PasswordHelper
+from user import User
+
+
+DB = DBHelper()
+PH = PasswordHelper()
 
 app = Flask(__name__)
 
+app.secret_key = 'De55Yb9QaIStU9xL/OcGUfP6+6UDjqolWzNsnJD/T0/VRMbbZs8hCKCOBmHIg+3hZEWuw0GUREx0uFMc077wfynkelP8kosMTjaf'
+
+login_manager = LoginManager(app)
+
 @app.route("/")
 def home():
-    return "Under construction"
+    return render_template("home.html")
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
+        user = User(email)
+        login_user(user, remember=True)
+        return redirect(url_for('account'))
+    return home()
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/register", methods=['POST'])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_form('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
